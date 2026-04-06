4c92249a01ad0a87d71f196f3afc5b4333b34946
Somchet Saengtawan <somchetsa@utacgroup.com>
2026-04-06 08:04:05 +0700

    update endpoint status

--- Controllers/CloudPrntController.cs
+++ Controllers/CloudPrntController.cs
@@ -7,12 +7,19 @@ namespace CloudPrntApi.Controllers;
 [ApiController]
 public class CloudPrntController : ControllerBase
 {
+    private readonly ILogger<CloudPrntController> _logger;
+    public CloudPrntController(ILogger<CloudPrntController> logger)
+    {
+        _logger = logger;
+    }
     [HttpGet("cloudprnt-setting.json")]
     public IActionResult Setting()
     {
+
+        _logger.LogInformation("Received request for cloudprnt-setting.json");
         return Ok(new
         {
-            pollingInterval = 5,
+            pollingInterval = 10,
             printJobUrl = "/cloudprnt/print",
             jobReady = true
         });
@@ -21,8 +28,19 @@ public class CloudPrntController : ControllerBase
     [HttpGet("cloudprnt/print")]
     public IActionResult Print()
     {
+        _logger.LogInformation("Received request for cloudprnt/print");
         var data = EscPosBuilder.BuildTestReceipt();
         return File(data, "application/octet-stream");
+    }
+
+    [HttpGet("cloudprnt/status")]
+    public IActionResult Status()
+    {
+        _logger.LogInformation("Received request for cloudprnt/status");
+        return Ok(new
+        {
+            status = "ok"
+        });
     }
 
 }
